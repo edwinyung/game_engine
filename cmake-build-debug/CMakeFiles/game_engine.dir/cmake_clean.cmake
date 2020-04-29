8e54ed9f6dd615950fa9a22e36cf7d95afdb5ce7
Edwin Yung <edwin@makespace.com>
2020-04-28 20:10:50 -0400

    initial build and image render with progress indicator

---
+++ cmake-build-debug/CMakeFiles/game_engine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_engine.dir/main.cpp.o"
+  "game_engine"
+  "game_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
