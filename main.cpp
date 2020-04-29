0a84b1272191df276179829d72c95104b7e5a579
Edwin Yung <edwin@makespace.com>
2020-04-28 23:34:56 -0400

    finish diffuse

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@
 
 #include <iostream>
 
-vec3 ray_color(const ray& r, const hittable& world) {
+vec3 ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + vec3(1,1,1));
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return vec3(0,0,0);
+
+    // minimum is not zero because of floating point approximation
+    if (world.hit(r, 0.001, infinity, rec)) {
+        vec3 target = rec.p + rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
@@ -20,6 +28,7 @@ void image() {
     const int image_width = 200;
     const int image_height = 100;
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
@@ -42,7 +51,7 @@ void image() {
                 auto u = (i + random_double()) / image_width;
                 auto v = (j + random_double()) / image_height;
                 ray r = cam.get_ray(u, v);
-                color += ray_color(r, world);
+                color += ray_color(r, world, max_depth);
             }
 
             color.write_color(std::cout, samples_per_pixel);
