c8504de282dd71eb19cbdb58fbacceeb600398f9
Edwin Yung <edwin@makespace.com>
2020-04-28 22:01:51 -0400

    multiple objects

---
+++ sphere.h
@@ -0,0 +1,52 @@
+//
+// Created by Edwin Yung on 4/28/20.
+//
+
+#ifndef GAME_ENGINE_SPHERE_H
+#define GAME_ENGINE_SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere: public hittable {
+    public:
+        sphere() {}
+        sphere(vec3 cen, double r) : center(cen), radius(r) {};
+
+        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
+
+    public:
+        vec3 center;
+        double radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
+    if (discriminant > 0) {
+        auto root = sqrt(discriminant);
+        auto temp = (-half_b - root)/a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.at(rec.t);
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
+            return true;
+        }
+        temp = (-half_b + root) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.at(rec.t);
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
+            return true;
+        }
+    }
+    return false;
+}
+
+#endif //GAME_ENGINE_SPHERE_H
